c93a968c8d8693de8d5411d810b223d64f6bdfe9
kiskikikuku <pthkuku99@gmail.com>
2022-06-27 11:00:24 +0900

    test prediction #2

--- basic_image.py
+++ basic_image.py
@@ -33,8 +33,59 @@ probability_model = tf.keras.Sequential([model, tf.keras.layers.Softmax()])
 
 prediction = probability_model.predict(test_images)
 
-print(prediction[0])
+def plot_image(i, predictions_array, true_label, img):
+    true_label, img = true_label[i], img[i]
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
 
-print(np.argmax(prediction[0]))
+    plt.imshow(img, cmap=plt.cm.binary)
 
-print(test_labels[0])
+    predicted_label = np.argmax(predictions_array)
+    if predicted_label == true_label:
+        color = 'blue'
+    else:
+        color = 'red'
+
+    plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
+                                         100*np.max(predictions_array),
+                                         class_names[true_label],
+                                         color=color))
+
+def plot_value_array(i, prediction_array, true_label):
+    true_label = true_label[i]
+    plt.grid(False)
+    plt.xticks(range(10))
+    plt.yticks([])
+    thisplot = plt.bar(range(10), prediction_array, color="#777777")
+    plt.ylim([0, 1])
+    predicted_label = np.argmax(prediction_array)
+
+
+i=0
+plt.figure(figsize=(6,3))
+plt.subplot(1,2,1)
+plot_image(i, prediction[i], test_labels, test_images)
+plt.subplot(1,2,2)
+plot_value_array(i, prediction[i], test_labels)
+plt.show()                                              #0번째 원소의 이미지, 예측, 신뢰도 점수 배열
+
+i=12
+plt.figure(figsize=(6,3))
+plt.subplot(1,2,1)
+plot_image(i, prediction[i], test_labels, test_images)
+plt.subplot(1,2,2)
+plot_value_array(i, prediction[i], test_labels)
+plt.show()                                              #12번째 원소의 이미지, 예측, 신뢰도 점수 배열
+
+num_rows = 5
+num_cols = 3
+num_images = num_rows*num_cols
+plt.figure(figsize=(2*2*num_cols, 2*num_rows))
+for i in range(num_images):
+    plt.subplot(num_rows, 2*num_cols, 2*i+1)
+    plot_image(i, prediction[i], test_labels, test_images)
+    plt.subplot(num_rows, 2*num_cols, 2*i+2)
+    plot_value_array(i, prediction[i], test_labels)
+plt.tight_layout()
+plt.show()                                               #5x3개 원소의 이미지, 예측, 신뢰도 점수 배열
